99a23cd988e53187cef995a82a4182a0aef54911
Hawk56767 <76022450+Hawk56767@users.noreply.github.com>
2022-10-23 19:50:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
 function preload(){
 
 }
@@ -10,13 +15,24 @@ poseNet=ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
 }
 function draw(){
+    document.getElementById("text_size").innerHTML="Width and Height of the text will be ="+difference+"px";
 background('#969A97');
+textSize(difference);
+fill('#FFE787');
+text("Hello", 50, 400);
 }
 function modelLoaded(){
 console.log('PoseNet is Initialized!');
 }
-function gotPoses(){
+function gotPoses(results){
 if(results.length > 0){
     console.log(results);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    console.log("Nose X= " + noseX+ "Nose Y=" + noseY);
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+    difference=floor(leftWristX-rightWristX);
+    console.log("leftWristX = "+leftWristX+"rightWristX = " + rightWristX + "difference ="+difference);
 }
 }
